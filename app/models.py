0a4ccaa69c1c7f6ae47d57c9abd95b1a9af06de2
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-07 01:33:07 +0300

    Added a password and email columns and hashing the password

--- app/models.py
+++ app/models.py
@@ -1,18 +1,33 @@
 from sqlalchemy import ForeignKey
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255), unique = True, index = True)
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password) :
+        self.pass_secure = generate_password_hash(password) 
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure, password)     
 
     def __repr__(self):
         return f'User{self.username}'
     
 class Role(db.Model):
-    __tablename__ = 'roles'  
+    __tablename__ = 'role'  
 
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(255))
