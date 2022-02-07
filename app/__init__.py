18e5f60a2cd038c94ca5be42c9c24f0d5c495232
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-07 22:27:42 +0300

    Initialized login manager, mail and configured photos

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,16 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+photos = UploadSet('photos', IMAGES)
+mail = Mail()
 
 #Initializing the application
 
@@ -22,6 +26,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     #Registering the blueprint
     from .main import main as main_blueprint
@@ -30,5 +35,8 @@ def create_app(config_name):
      #Registering auth blueprint
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
+
+    #configure UploadSet
+    configure_uploads(app, photos)
 
     return app
