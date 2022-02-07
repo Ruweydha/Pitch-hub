096f9c212b5afecff070bc19ed2f38b1fff30593
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-07 22:34:30 +0300

    Added code for updating bio and profile pic

---
+++ app/main/views.py
@@ -0,0 +1,48 @@
+from flask import redirect, render_template, url_for, abort,request
+from flask_login import login_required
+from . import main
+from ..models import User
+from .forms import UpdateProfile
+from .. import db, photos
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/user/<uname>') 
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template('profile/profile.html', user = user)    
+
+@main.route('/user/<uname>/update', methods = ['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile() 
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()  
+
+        return redirect(url_for('.profile', uname = user.username)) 
+    return render_template('profile/update_profile.html', form = form)
+
+@main.route('/user/<uname>/update/pic', methods = ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()  
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photos'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+
+    return redirect(url_for('main.profile', uname = uname))    
