1ecf272fd0510e91627da7a0689ff7c4039263f7
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-07 22:29:02 +0300

    Added profile pic and bio column

--- app/models.py
+++ app/models.py
@@ -2,6 +2,11 @@ from sqlalchemy import ForeignKey
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -10,6 +15,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255), unique = True, index = True)
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
 
     @property
