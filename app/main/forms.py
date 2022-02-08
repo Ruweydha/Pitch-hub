9ee6d4f2d11b29cb521bff1817438dca5e85e7f1
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-08 14:52:22 +0300

    Created a form for submiting pitches

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
+from wtforms import TextAreaField, SubmitField, StringField
 from wtforms.validators import DataRequired
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField("Tell us something about yourself", validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class PitchesForm(FlaskForm):
+
+    title = StringField('Pitch title', validators=[DataRequired()])
+    content = TextAreaField('Write your pitch', validators=[DataRequired()])
+    submit = SubmitField('Submit')    
