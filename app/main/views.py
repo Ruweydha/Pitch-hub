f9ccfd7768867e967ba738f84f6b0682714f01b1
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-08 14:53:44 +0300

    Created aview for submitting pitches and updated the profile one to display the users pitches

--- app/main/views.py
+++ app/main/views.py
@@ -1,27 +1,31 @@
+from os import uname
 from flask import redirect, render_template, url_for, abort,request
-from flask_login import login_required
+from flask_login import current_user, login_required
 from . import main
-from ..models import User
-from .forms import UpdateProfile
+from ..models import User, Pitches
+from .forms import UpdateProfile, PitchesForm
 from .. import db, photos
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    pitches = Pitches.query.all()
+    return render_template('index.html', pitches = pitches)
 
 @main.route('/user/<uname>') 
 def profile(uname):
     user = User.query.filter_by(username = uname).first()
+    pitches = Pitches.get_pitch(user.id)
 
     if user is None:
         abort(404)
 
-    return render_template('profile/profile.html', user = user)    
+    return render_template('profile/profile.html', user = user, pitches = pitches)    
 
 @main.route('/user/<uname>/update', methods = ['GET', 'POST'])
 @login_required
 def update_profile(uname):
     user = User.query.filter_by(username = uname).first()
+    
     if user is None:
         abort(404)
 
@@ -45,4 +49,19 @@ def update_pic(uname):
         user.profile_pic_path = path
         db.session.commit()
 
-    return redirect(url_for('main.profile', uname = uname))    
+    return redirect(url_for('main.profile', uname = uname))   
+
+@main.route('/user/pitch/new/<int:id>', methods =['GET', 'POST'])
+@login_required
+def new_pitch(id):
+    form = PitchesForm()
+    user = User.query.filter_by(id = id).first()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        new_pitch = Pitches(title = title, content = content, pitcher = current_user)
+        new_pitch.save_pitch()
+        return redirect(url_for('.profile', uname = user.username ))
+
+    return render_template('new_pitch.html', pitch_form = form, user = user.username)
