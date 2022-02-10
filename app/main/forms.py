ba24b038aeb51c85d069d80d616522bd5a41b0b7
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-10 06:48:13 +0300

    Added a form for getting comments

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,7 @@
+from unicodedata import category
+import flask_simplemde
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField, StringField
+from wtforms import TextAreaField, SubmitField, StringField , SelectField
 from wtforms.validators import DataRequired
 
 class UpdateProfile(FlaskForm):
@@ -9,5 +11,10 @@ class UpdateProfile(FlaskForm):
 class PitchesForm(FlaskForm):
 
     title = StringField('Pitch title', validators=[DataRequired()])
+    category = SelectField("Category", choices =[(1,'Technology'),(2,'Fitness'),(3,'Interview'),(4,'Product pitch')],validators=[DataRequired()])
     content = TextAreaField('Write your pitch', validators=[DataRequired()])
     submit = SubmitField('Submit')    
+
+class CommentsForm(FlaskForm):
+    comment = TextAreaField('Write your comment')
+    submit = SubmitField('Submit')
